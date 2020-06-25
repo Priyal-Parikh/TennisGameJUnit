b92a7be89db278500826f082551ead91ab0fd34a
Priyal Parikh <parikhpriyal91@yahoo.com>
2020-06-25 12:11:50 +0530

    First Successful Test case for Nil score
    First Successful Test case for Nil score

--- src/TennisGame.java
+++ src/TennisGame.java
@@ -4,41 +4,81 @@ public class TennisGame {
 	private int player1_scrore = 0;
 	private int player2_scrore = 0;
 	private String player1_name = null;
-	private String player2_name =null;
-	
-	
-	public TennisGame(String player1_name, String player2_name) {
+	private String player2_name = null;
+
+	public TennisGame(int player1_scrore, int player2_scrore, String player1_name, String player2_name) {
+		this.player1_scrore = player1_scrore;
+		this.player2_scrore = player2_scrore;
 		this.player1_name = player1_name;
 		this.player2_name = player2_name;
 	}
+
+	
+	// Method added to get the score at any gven time
+	public String calculateScore() {
+		return convertToReadableScore(player1_scrore) + "," + convertToReadableScore(player2_scrore);
+	}
+
+	
+	// Method added to covert Score into readable format
+	public String convertToReadableScore(int score) {
+
+		// System.out.println("Score provided is:"+score);
+		String scoreInWord = "";
+
+		switch (score) {
+		case 0:
+			scoreInWord = "Love";
+			break;
+		case 1:
+			scoreInWord = "Fifteen";
+			break;
+		case 2:
+			scoreInWord = "Thirty";
+			break;
+		case 3:
+			scoreInWord = "Forty";
+			break;
+
+		default:
+			scoreInWord = "Invalid Input of Score";
+		}
+
+		return scoreInWord;
+	}
 	
 	
+	//getters and setters
 	public int getPlayer1_scrore() {
 		return player1_scrore;
 	}
+
 	public void setPlayer1_scrore(int player1_scrore) {
 		this.player1_scrore = player1_scrore;
 	}
+
 	public int getPlayer2_scrore() {
 		return player2_scrore;
 	}
+
 	public void setPlayer2_scrore(int player2_scrore) {
 		this.player2_scrore = player2_scrore;
 	}
+
 	public String getPlayer1_name() {
 		return player1_name;
 	}
+
 	public void setPlayer1_name(String player1_name) {
 		this.player1_name = player1_name;
 	}
+
 	public String getPlayer2_name() {
 		return player2_name;
 	}
+
 	public void setPlayer2_name(String player2_name) {
 		this.player2_name = player2_name;
 	}
-	
-	
-	
-	
+
 }
--- src/TennisGameTest.java
+++ src/TennisGameTest.java
@@ -1,5 +1,14 @@
 import junit.framework.TestCase;
 
 public class TennisGameTest extends TestCase{
+	
+	TennisGame tennisGame = new TennisGame(0,0,"Player A" , "Player B");
+	
+	public void testNewGameReturnLoveAll()
+	{
+		String score = tennisGame.calculateScore();
+		
+		assertEquals("Love,Love", score);
+	}
 
 }
